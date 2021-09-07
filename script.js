7d19db1a8aa112edbcd74464597e00b73cdd715c
Furentsu <eugeniofranzoso@gmail.com>
2021-09-07 16:35:41 +0200

    Added the last part of the exercise

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
+
+
+
+
+
+
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
-// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-// Creare un array di oggetti di studenti.
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-
-
 let myStudent = {
     'nome': undefined,
     'cognome': undefined,
     'eta': undefined,
 } 
 
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 for (let key in myStudent) {
     console.log(myStudent[key]);
 }
 
+// Creare un array di oggetti di studenti.
 let myStudentsArray = [
     {
         'name': "Akira",
@@ -41,9 +43,17 @@ let myStudentsArray = [
         'surname': "McFly",
         'age': "Immortal",
     }
-
 ]
 
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 for (let i=0; i < myStudentsArray.length; i++) {
     console.log(myStudentsArray[i].name + ' ' + myStudentsArray[i].surname);
 }
+
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+let chosenName = prompt("Choose a name");
+let chosenSurname = prompt("Choose a surname");
+let chosenAge = prompt("Choose an age");
+
+myStudentsArray.push({name:chosenName, surname:chosenSurname, age:chosenAge});
+console.log(myStudentsArray);
